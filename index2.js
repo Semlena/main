0fcd65dfa025cd79743b1c7cd7e9cd612cd9a724
Semlena <semlena1812@yandex.ru>
2016-11-11 19:08:24 +0400

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+function url(url){
+	const reg= new RegExp('@?(https?:)?(\/\/)?((telegram|vk|vkontaktr)[^\/]*\/)?([a-zA-Z0-9]*)','i');
+	const username=url.match(reg);
+	name=(username)?username:'Invalid username';
+	return name;
+}
+
+app.get('/2C',(reg,res) => {
+	const name = url(reg.query.url);
+	res.json({
+		url:reg.query.url,
+	    name,
+	});
+});
+
+app.listen(3000, () => {
+  console.log('Your app listening on port 3000!');
+});
