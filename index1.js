621a84d115fd48288f67a74f47968550cccacfca
Semlena <semlena1812@yandex.ru>
2016-12-11 18:44:03 +0400

    Update index1.js

--- index1.js
+++ index1.js
@@ -2,25 +2,25 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 function user(name){
 	
 	const name_rep=name.replace(/\s/ig, ',');
     const name_array=name_rep.split(',');
     const length=name_array.length;
-	const user_name=((length==3)?(name_array[2]+" "+name_array[0][0]+"."+" "+" "+name_array[1][0]+"."):(" ")).toString();
-    const user_name1=((length==2)?(name_array[1]+" "+name_array[0][0]+"."):(" ")).toString();
-	const user_name2=((length==1)?(name_array[0]+" "):(" ")).toString();
+	const user_name=(length==3)?(name_array[2]+" "+name_array[0][0]+"."+" "+name_array[1][0]+"."):(" ");
+    const user_name1=(length==2)?(name_array[1]+" "+name_array[0][0]+"."):(" ");
+	const user_name2=(length==1)?(name_array[0]+" "):(" ");
 	const user=user_name+user_name1+user_name2;
-	const res=(user=="   ")?("Invalid fullname"):user;
-	return res;
+	const res=(user=="   ")?("Invalid fullname"):user.toString();
+	return res.trim();
 }
 
 app.get('/2B',(reg,res) => {
 	const name = reg.query.fullname;
-	//const reg_name=regName(name);
-	const user_name=user(name);
-	res.send(user_name);
+	const username=(name)?user(name):'Invalid fullname';
+	res.send(username);
 });
 
 app.listen(3000, () => {
