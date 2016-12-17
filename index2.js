b0ab0e474bb832897f7bb37bd93cd4353593d398
Semlena <semlena1812@yandex.ru>
2016-12-17 14:05:51 +0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -2,20 +2,21 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
+
 function url(url){
-	const reg= new RegExp('@?(https?:)?(\/\/)?(www.)?((telegram||vk||vkontakt||twitter)[^\/]*\/)?([a-zA-Z0-9]*)','i');
+	const reg= new RegExp('@?(https?:)?(\/\/)?(www.)?((telegram||vk||vkontakt||twitter)[^\/]*\/)?(@?[a-zA-Z0-9]*\.*\-*)','i');
 	const username=url.match(reg);
-	const user=username[5];
+	const l=username.length-1;
+	const u_name=username[l].split("/");
+	const user=u_name[0];
+	//const user=user_name.replace(/@/g,"");
 	const name=(user)?("@"+user):'Invalid username';
 	return name.toString();
 }
-
-app.get('/2C',(reg,res) => {
-	const name = url(reg.query.username);
-	res.json({
-		url:reg.query.username,
-	    name,
-	});
+app.get('/2С',(reg,res) => {
+	const name = url(reg.query.username)
+	res.send(name);
 });
 
 app.listen(3000, () => {
