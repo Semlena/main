f2bdfe6a043a453987f8d4d6446b22f4ec92e998
Semlena <semlena1812@yandex.ru>
2016-12-17 14:11:08 +0400

    Update index1.js

--- index1.js
+++ index1.js
@@ -6,10 +6,10 @@ app.use(cors());
 
 
 function sym(name){
-const re=/[0-9]|_|-|%|\/|\}|\,|\"|@/g;
-	const symbol=name.match(re);
-	const result=(symbol)?true:false;
-	return result;
+    const re=/[0-9]|_|-|%|\/|\}|\,|\"|@/g;
+    const symbol=name.match(re);
+    const result=(symbol)?true:false;
+    return result;
 }
 function Uppercase(name){
 	const re=new RegExp(/[-А-Я]/);
@@ -38,25 +38,25 @@ function register(name){
 
 
 function user(name){
-	const n=name.trim();
-	const VRegExp = new RegExp(/(\s{2,})/g); 
+    const n=name.trim();
+    const VRegExp = new RegExp(/(\s{2,})/g); 
     const VResult = n.replace(VRegExp," ");
-	const name_rep=VResult.replace(/\s/ig, ',');
+    const name_rep=VResult.replace(/\s/ig, ',');
     const name_array=name_rep.split(',');
     const length=name_array.length;
-	const user_name=(length==3)?(register(name_array[2])+" "+Uppercase(name_array[0][0])+"."+" "+Uppercase(name_array[1][0])+"."):(" ");
+    const user_name=(length==3)?(register(name_array[2])+" "+Uppercase(name_array[0][0])+"."+" "+Uppercase(name_array[1][0])+"."):(" ");
     const user_name1=(length==2)?(register(name_array[1])+" "+Uppercase(name_array[0][0])+"."):(" ");
-	const user_name2=(length==1)?(register(name_array[0])+" "):(" ");
-	const user=user_name+user_name1+user_name2;
-	const res=(user=="   ")?("Invalid fullname"):user.toString();
-	return res.trim();
+    const user_name2=(length==1)?(register(name_array[0])+" "):(" ");
+    const user=user_name+user_name1+user_name2;
+    const res=(user=="   ")?("Invalid fullname"):user.toString();
+    return res.trim();
 }
 
 app.get('/2B',(reg,res) => {
-	const name = reg.query.fullname;
-	const symbol=sym(name);
-	const username=((name)&&(!symbol))?user(name):'Invalid fullname';
-	res.send(username);
+    const name = reg.query.fullname;
+    const symbol=sym(name);
+    const username=((name)&&(!symbol))?user(name):'Invalid fullname';
+    res.send(username);
 });
 
 app.listen(3000, () => {
